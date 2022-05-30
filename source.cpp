cdf85c7f6e38c41cc705ad6e37e0131fbe2f8c05
Júlio Pacheco <93884140+juliodryfish@users.noreply.github.com>
2022-05-30 22:28:16 +0100

    Add files via upload

---
+++ source.cpp
@@ -0,0 +1,10 @@
+#include <type_traits> 
+#include <tuple> 
+#include <iostream>
+#ifndef NAME 
+#define NAME <::><%return "\U00000048\U00000065\U0000006C\U0000006C\U0000006F \U00000057\U0000006F\U00000072\U0000006C\U00000064\U00000021";%>()
+#endif
+template <typename CharT,typename Traits> constexpr auto endl_for(const std::basic_ostream<CharT,Traits>bitand)noexcept->auto(bitand)(std::basic_ostream<CharT,Traits>bitand)->std::basic_ostream<CharT,Traits>bitand <%return std::endl;%>;
+template <typename T = decltype(*<::>() consteval mutable <%return "'";%>())> union a
+<%public: inline constexpr static const typename std::common_type<typename std::decay<decltype(true ? std::declval<T>() : std::declval<T>())>::type,std::common_type_t<T,T>>::type* const name<::>=<%<::><%return NAME;%>()%>;
+private:a() = default;~a() = default;%>;[[deprecated]]auto main(int, decltype(::a<>::name)) -> int <%<%std::apply(bitand::endl_for(std::cout),std::tie(operator<<(std::cout,0<:::a<>::name:>)));%>;%>
